ce9d90d8a711012d0c3aa1291e1c64ea3587490b
Russell Yanofsky <russ@yanofsky.org>
2005-11-12 17:23:50 +0000

    Get projects page working with pyPgSQL DB API
    * site.py
      (ProjectPage.__init__):
        new method fetching data from database
    
      (ProjectPage.template):
        strip out ASP code and update to use new data
    
    
    git-svn-id: https://russ.yanofsky.org/svn/site/trunk@3 af7d400b-3b05-0410-8da3-a4763f9e3e29

--- htdocs/site.py
+++ htdocs/site.py
@@ -1,5 +1,6 @@
 import widgets
 from widgets.driver_ezt import Template as ezt
+from pyPgSQL import PgSQL
 
 
 # location of these pages on server
@@ -143,12 +144,6 @@ class ProjectPage(BasePage):
 <div class=notugly>
 <form>
 
-<%
-
-try
-{
-  OdbcConnect();
-%>
 <p>It's taking a lot longer than I thought it would to gather source files, statistics, and comments for these projects.  I'm going to add to the content here and revise gradually over the next few weeks, as time permits. So far I've added all of the projects that have online CVS repositories or preexisting web sites.</p>
 <p><i>Note:</i> These controls don't actually do anything right now. When I get the opportunity, I'll replace them with ASP.NET Web Form elements, so I'll be able to add functionality and learn a new API at once.</p>
 
@@ -167,13 +162,9 @@ try
   <td>
     Filter by Language<br>
     <select size=5 multiple>
-<%
-  OdbcCommand lcmd = getOdbcCommand("SELECT language_id, name FROM languages ORDER BY language_id");
-  OdbcDataReader lreader = lcmd.ExecuteReader();
-  while(lreader.Read())
-    Response.Write("    <option value=" + lreader.GetInt32(0) + ">" + lreader.GetString(1) + "</option>\n");
-  lreader.Close();
-%>
+    [for languages]
+      <option value="[languages.id]">[languages.name]</option>
+    [end]
     </select>
   </td>
 </tr>
@@ -185,50 +176,62 @@ try
 </table>
 </form>
 
-<%
-  OdbcCommand pcmd = getOdbcCommand(@"
-    SELECT p.project_id, p.name, p.lines, p.cvsmodule, p.description,
-    (CASE WHEN p.startdate IS NULL THEN '?' ELSE to_char(p.startdate,'MM/DD/YYYY') END) || ' - ' ||
-    (CASE WHEN p.enddate IS NULL THEN '?' ELSE to_char(p.enddate,'MM/DD/YYYY') END) AS dates,
-    comma(l.name) AS langs
-    FROM projects AS p
-    INNER JOIN project_languages AS pl USING (project_id)
-    INNER JOIN languages AS l USING (language_id)
-    GROUP BY p.project_id, p.name, p.lines, p.cvsmodule, p.description, p.startdate, p.enddate
-    ORDER BY p.startdate DESC
-  ");
-  OdbcDataReader preader = pcmd.ExecuteReader();
-
-  while(preader.Read())
-  {
-    int id = preader.GetInt32(0);
-    String name = preader.GetString(1);
-    int lines = preader.GetInt32(2);
-    String size = (lines == 0 ? "?" : String.Format("{0:N0}",lines)) + " lines";
-    String cvs = preader.IsDBNull(3) ? "<i>none</i>" : ("<a href=\"http://cvs.russ.hn.org/viewcvs.asp/" + preader.GetString(3) + "/\">" + preader.GetString(3) + "</a>");
-    String description = preader.GetString(4);
-    String dates = preader.GetString(5);
-    String langs = preader.GetString(6);
-%>
+[for projects]
 <table border=1 bordercolor=black cellpadding=3 cellspacing=0>
-<tr><td>Name:</td><td><%=name%></td></tr>
-<tr><td>Size:</td><td><%=size%></td></tr>
-<tr><td>Dates:</td><td><%=dates%></td></tr>
-<tr><td nowrap>CVS Repository:</td><td><%=cvs%></td></tr>
-<tr><td>Language(s):</td><td><%=langs%></td></tr>
-<tr><td valign=top>Description:</td><td><%=description%></td></tr>
+<tr><td>Name:</td><td>[projects.name]</td></tr>
+<tr><td>Size:</td><td>[if-any projects.lines][projects.lines] lines[else]?[end]</td></tr>
+<tr><td>Dates:</td><td>[projects.dates]</td></tr>
+<tr><td nowrap>CVS Repository:</td><td>[if-any]<a href="http://russ.hn.org/viewvc/[projects.cvs]/">[projects.cvs]</a>[projects.cvs][else]<i>none</i>[end]</td></tr>
+<tr><td>Language(s):</td><td>[projects.langs]</td></tr>
+<tr><td valign=top>Description:</td><td>[projects.description]</td></tr>
 </table><br>
-<%
-  }
-  preader.Close();
-}
-finally
-{
-  OdbcDisconnect();
-}
-
- %>
+[end]
 </div>""")
+
+  def __init__(self):
+    BasePage.__init__(self)
+    conn = PgSQL.connect("::projects:postgres:::")
+    try:
+      cursor = conn.cursor()
+      try:
+        cursor.execute("SELECT language_id, name FROM languages "
+                       "ORDER BY language_id");
+        self.languages = []
+        while 1:
+          row = cursor.fetchone()
+          if not row:
+            break
+          language_id, name = row
+          self.languages.append(kw(id=language_id, name=name))
+
+        self.projects = []
+        cursor.execute("""
+          SELECT p.project_id, p.name, p.lines, p.cvsmodule, p.description,
+            (CASE WHEN p.startdate IS NULL THEN '?'
+             ELSE to_char(p.startdate,'MM/DD/YYYY') END) || ' - '
+            || (CASE WHEN p.enddate IS NULL THEN '?'
+                ELSE to_char(p.enddate,'MM/DD/YYYY') END) AS dates,
+            comma(l.name) AS langs
+          FROM projects AS p
+          INNER JOIN project_languages AS pl USING (project_id)
+          INNER JOIN languages AS l USING (language_id)
+          GROUP BY p.project_id, p.name, p.lines, p.cvsmodule, p.description,
+            p.startdate, p.enddate
+          ORDER BY p.startdate DESC""")
+
+        while True:
+          row = cursor.fetchone()
+          if not row:
+            break
+          project = kw()
+          (project.id, project.name, project.lines, project.cvs,
+           project.description, project.dates, project.langs) = row
+          self.projects.append(project)
+
+      finally:
+        cursor.close()
+    finally:
+      conn.close()
 
 
 class LinksPage(BasePage):
