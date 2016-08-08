735a03a97b9d2dd744ba925cedbc9d0f618fd5d4
Russell Yanofsky <russ@yanofsky.org>
2016-08-08 02:55:03 +0000

    Update pgp.php from PHP 4.x->7.x, other fixes
    - Stop using global $POST variables
    - Switch from $HTTP_X_VARS arrays to $_X arrays.
    - Add isset checks to avoid PHP warnings.
    - Switch to newer PGP key.
    - Get rid of dumb code that writes plaintext to file and tries to
      wipe it. Instead pipe plaintext directly to gpg.
    - Fix HTML escaping bugs / XSS vulns with htmlspecialchars
    - Avoid email header escaping bugs by embedding from/subject text
      in encrypted message.
    - Drop <form> action attribute path when submitting to self.
    - Drop note about https not providing authentication, since letsencrypt
      certs are better than self signed certs for this.
    - Change form submission message to show plaintext instead of
      ciphertext.
    
    git-svn-id: https://russ.yanofsky.org/svn/site/trunk@86 af7d400b-3b05-0410-8da3-a4763f9e3e29

--- htdocs/pgp.php
+++ htdocs/pgp.php
@@ -18,56 +18,38 @@ body
 
 <body bgcolor="#19334C" text="#FFFFFF" link="#FFFFFF" vlink="#FFFFFF" alink="#FFFFFF">
 
-<? 
+<?php
 
-function param(&$arg)
+if (isset($_POST["text"]))
 {
-  if (!isset($arg)) $arg = false;
-}
-
-if (isset($HTTP_POST_VARS["text"]))
-{
-
   flush();
 
-  param($text);
-  param($from);
-  param($subject);
+  $text = $_POST["text"];
+  $from = $_POST["from"];
+  $subject = $_POST["subject"];
+
+  $out = tempnam("", "");
+  unlink($out);
   
-  $in = tempnam("M:/server/shares/temp","secret");
-  $out = tempnam("M:/server/shares/temp","secret");
-  
-  $ip = $HTTP_SERVER_VARS["REMOTE_ADDR"];
+  $ip = $_SERVER["REMOTE_ADDR"];
   $dn = gethostbyaddr($ip);
-  $us = $HTTP_SERVER_VARS["HTTP_USER_AGENT"];
-  
-  $fin = fopen($in,"wb");
+  $us = $_SERVER["HTTP_USER_AGENT"];
+
+  $cmd = "gpg -q -a --batch --home /var/lib/russ/gpg -o " . escapeshellarg($out) . " --encrypt --recipient 164650CC > /tmp/err 2>&1";
+  $fin = popen("$cmd", "w");
+  fwrite($fin, "Subject = $subject\nFrom = $from\n");
   fwrite($fin, "IP Address = $ip\nHost = $dn\nHTTP_USER_AGENT = $us\n");
   fwrite($fin,$text);
   fclose($fin);
   
-  unlink($out);
-  passthru("gpg -q -a --batch --home M:/server/bin/php/gpg -o \"$out\" --encrypt --recipient 5FAA0216 \"$in\"");
-  
   $fout = fopen($out,"r");
-  $text = fread($fout,filesize($out));
+  $ctext = fread($fout,filesize($out));
   fclose($fout);
-
-  $bsize = 1024;
-  $passes = (int)ceil((double)filesize($in) / (double)$bsize);
-  $nulls = str_repeat("\x00",$bsize);
-  
-  $fin = fopen($in, "wb");
-  for($i = 0; $i < $passes; ++$i)
-    fwrite($fin,$nulls);
-  fclose($fin);
-
-  unlink($in);
   unlink($out);
   
-  mail("rey4@columbia.edu",$subject,$text, "To: Russell Yanofsky <rey4@columbia.edu>\nFrom: $from\nX-Mailer: PHP/" . phpversion());
+  mail("russ+gpgpgp@yanofsky.org", "gpg.php mail", $ctext, "X-Mailer: PHP/" . phpversion(), "-f russ+gpgphp@yanofsky.org");
   
-  print("<h3>Message Sent</h3><pre><b>From:    $from\nSubject: $subject</b>\n\n$text</pre>");
+  print("<h3>Message Sent</h3><pre><b>From:    " . htmlspecialchars($from) . "\nSubject: " . htmlspecialchars($subject) . "</b>\n\n" . htmlspecialchars($text) . "</pre>");
 
 } else {
 
@@ -86,45 +68,55 @@ Two free, popular software packages that support it are
 <form>
 <textarea id="key" rows=10 cols=65 READONLY>
 -----BEGIN PGP PUBLIC KEY BLOCK-----
-Version: GnuPG v1.0.6 (MingW32)
-Comment: For info see http://www.gnupg.org
 
-mQGiBDsmwuMRBADIjZf9Sfub8z8nTWTwJmEKJ2Z5vj79GvhRmAnMDSY/EngOp+Vd
-b33xOI8EEysOnX9wrmrMGmDE4jrE54zXS1kQHZSrvm1LFH4tjDBAM6j/jr2RWr+H
-6VhhbyEn3ET/jD43TZThBgxrTGUVb3BwJi1RL6SOgqyFGcte+G9tJC6JkQCg/wsh
-RLYEUl7X06ch9ghl7Z1CLXkD/iEINLLE+fiMNorXRyjoGnqqYaFmfXwyWPsubXHN
-fXH8M1BfDnQzUb++POw5eNZcQkqyyCFKMr9FrYJkLbNWpqNuGYnVGpOaOQX7aojs
-H71c8/+b9Z9H+Q2ThAy71rUEQb9jqQPtAkw+PEQukXvHCuK1KePMKKKL5sf2aT5/
-/bTEA/9f6/ed9J6Y2rTVpKg3CUrqQfLXKJqDDi0BM3F3aba1ahmeZFa8feo0gY+V
-s6TB1CuTRp8WnUhPwcd8wDCEilRzb9H/Hg3DJHdlRexJxFetqliYJxI6TUELtbHW
-EYaK59fQGqTlwfyJbxUmu836q4GzOx0WMxYVi6vnZ3aa1fckbbQkUnVzc2VsbCBZ
-YW5vZnNreSA8cmV5NEBjb2x1bWJpYS5lZHU+iFgEEBECABgFAjsmwuMICwMJCAcC
-AQoCGQEFGwMAAAAACgkQXsk07l+qAhYcHQCgsAO9bzb2yLWjIJX2KCoN4abKSmAA
-oMrIZpnwaZWAKPbSiLrpsCe9HJYeuQQNBDsmwuQQEAD5GKB+WgZhekOQldwFbIeG
-7GHszUUfDtjgo3nGydx6C6zkP+NGlLYwSlPXfAIWSIC1FeUpmamfB3TT/+OhxZYg
-TphluNgN7hBdq7YXHFHYUMoiV0MpvpXoVis4eFwL2/hMTdXjqkbM+84X6CqdFGHj
-hKlP0YOEqHm274+nQ0YIxswdd1ckOErixPDojhNnl06SE2H22+slDhf99pj3yHx5
-sHIdOHX79sFzxIMRJitDYMPj6NYK/aEoJguuqa6zZQ+iAFMBoHzWq6MSHvoPKs4f
-dIRPyvMX86RA6dfSd7ZCLQI2wSbLaF6dfJgJCo1+Le3kXXn11JJPmxiO/CqnS3wy
-9kJXtwh/CBdyorrWqULzBej5UxE5T7bxbrlLOCDaAadWoxTpj0BV89AHxstDqZSt
-90xkhkn4DIO9ZekX1KHTUPj1WV/cdlJPPT2N286Z4VeSWc39uK50T8X8dryDxUcw
-Yc58yWb/Ffm7/ZFexwGq01uejaClcjrUGvC/RgBYK+X0iP1YTknbzSC0neSRBzZr
-M2w4DUUdD3yIsxx8Wy2O9vPJI8BD8KVbGI2Ou1WMuF040zT9fBdXQ6MdGGzeMyEs
-tSr/POGxKUAYEY18hKcKctaGxAMZyAcpesqVDNmWn6vQClCbAkbTCD1mpF1Bn5x8
-vYlLIhkmuquiXsNV6z3WFwACAhAAzf7IGdQmGbNXgyUH+M2sX/RpOjn5foqPaLQB
-UmSbDGv6yfm/I5q86Rf09KyNCQzRXXjzEwAkHkbGzGy7DP9oBLFFCPb4DVnp8xCi
-2AI446Y42fHEOUaiRB44R0kNQtlutO7n17eDQhVLpqvSMzmutIJ1yfZGYAtCpTPe
-YHYreRz/U239yJbEv2iCSGjDPNwU3BWn+GoOJAXavKGPU78ynn9BTF5LGhqXt8sV
-xDgO1s4IrYyrvg9LDjhUuQoLrzd1Kduc8y9DbgBs6YuSLxJt5zwHoFj8Dsjm7yLI
-Vs/CGroCiHcuyE1XKtmDh5pc1ZhnXa+rmpXur37NTrYE9mja6AfKU+g5x2A0q7kf
-wC/gcyjo9AU5BxsUbATo0k+mV6Dh0x350Zk6+StfJIFOXzldrLUdxvRvVNaLbWsT
-0lb/cPyehkHB8WnhMWDkLi0kUL4Krj+unGXAs0xn5av8FcO48NrUUi+sbrJtujkK
-3ysi9wwyTGk66jEf5bbYhdMCcyXa7k8lI48fSkfJYz+XABZ0y+sEn4gkYSEjVa5n
-hN+lN129e7+YC9EebHzkh0X9TIIyrCaunSosCTYs4ymb64E0EsNWqUuWEDkvAxlh
-HZTlZuzEZjgR5oU7sWGgrusOPqYoSTO/QQ2Pzx4RZnOAVm775+ZUs9+rXXBBzSnE
-kZgiQjGITAQYEQIADAUCO5a9AAUJAlEtnAAKCRBeyTTuX6oCFoNtAJ9gQxTuHB+/
-fLgB4O0s9pHrkHtntQCg03PS7hIhxJe3pzAfXLWK0CS5kF8=
-=D+Gl
+mQINBFcSZZMBEADhObc3Z2EOZU5gQzWu08iHPHW2t3EBPl+zGiQRzOdfWuX0N0EW
+CncXAS4g4RvPF5ZthancLl+v1bu7F1cT7cTL+bT6Wm6dHOQXVoDRmTdSX51T7V9F
+2wawd43y+F9bwaIY2Xn/Vliw2QLRAiaV3NoR+9Ufm8A8ULZEix0UPUgJkxa9DRq4
+5Z+VYtCMzsNTzmxsRB8NetxFq3uTfCwFHV+rNqRunn5Nwwjv1FxbeucnmAGx1bR8
+I5QcoNbAhPoYi2HYX7sLyP078CuI+9MMor2bnGQdCk6LCAtjWRW3MmNiebP7etfc
+uAxMzzUR/AXErM/EleX+tayZspZAwTsA+iW0EiHcUw3bf++k1qYN1fAlCjFgQy8e
+x3CeEkynHTlkSiVCVa3wplftzt23zVuBh4e8O46fO7/1fA+XMz0OBwYHqSc3+7M2
+q8HywXTDKYKKlDvOBYkawEmjuVxzdW+1MYMhtUlLjSnSq8MbjEZV2colDx9R8cqz
+J/fB2vNOYscC8f0uJgjj78N9gxD+d1IzAeL8OHL3HumZ31F3rJKzJFAoM0SQxGCI
+HMxIYHmJhLzbjMXWF+ePRVT/L+0inLCRkHtwbmrTb5D2fcbUn4BYoi0DQfrKmGkr
+5cofDbTGxVAYEmB7siHr0nkA1/Th+KQVJktvIRske3nHY3S0QthfOrPk3QARAQAB
+tCRSdXNzZWxsIFlhbm9mc2t5IDxydXNzQHlhbm9mc2t5Lm9yZz6JAj0EEwEKACcF
+AlcSZZMCGwMFCQHhM4AFCwkIBwMFFQoJCAsFFgIDAQACHgECF4AACgkQb9FfSxZG
+UMyUNxAAuSbIq91dzmJVvmv1z5SsyWWL/l9UEGeArj2BOHD/DtUEZZzxwHKESfJG
++/dXwEA0JUHBPaACXHq9VZYz8/jBIR3I9CxGzr+qoxiBASjZ2b2A4mfzgWvG9OL+
+kSzIE1qke4f9X1MkWmHl0fwtG0UoEi1H4sItfXOgsZmwRyy6B4ctwoco4bD/TSsO
++GhIakweHSEyDbx9kNz58yy6o7bqyXtzUP4r9aWRFoH08cR3jVfyjbJMT5xnse4l
+k41yD2Wl01vu7vOb4/9W3NwbK9RGplPJ6BP+hxpadCfuNlvV5NIuFpwU33FAUJiJ
+YHxcddhMG/XUOyKz1RPhR73GLHlgjxIdD1byj4l6RTQKo2GYIr3s/tCk5zJb5S+p
+HbmsJWztsYoVwh4+MpueO++c6cs8+hBe+Pk/AVOzzBxkI2B/r1DY4AFgLeNn3f7L
+1o2hAIxUxUp1UlTvLWHu7iqaylsCRSeocY8R8AX69h5TCRBHtO2T7saGM2DVgolo
+dG/rwvnFAbZDvcwmH1EohOdMB//qGcEIYRd9e92Ce//HPI+QwBfVaYGQtQmNT/jk
+YksBnvPfIXUqzwBdASo89rRKhdFfV1TdCSxxyMFD1TAFTeAW9uaMy3l75+S1i5co
+W7gtwtejuBuEWUAK/omvBXy5Srf7hGVdpH2STyPmv4f4H5epW3e5Ag0EVxJlkwEQ
+AKh4DWR2Q56IRZt7+jXrcq+LmzVMALmRKHmYQuoFO4XW5IMmzdWHtL58IHvYihDW
+YU6P32eLLfoj8tm+DwGbkxwgYgP6Ul7JK/YirnzMCzBRFretuJwFUhDrZWRCg6y8
+Yk97xl6z81XwjNdVLpoqCGEulzfOKD+vK85RZC23uZ67NXnLUC+z0WnkxOBcmbsq
+pgZjW/lTBei5qFgFDRCJydivpwp9r5Gj1FNfK6xKY1UuIr06TxSSQ2gKfeGYt8p0
+ibMPMKKdA4yiLJm2Y0jkBPyZEHWrWDqMe79ZxdmFMELQFXRwiUasDUS+xkpBMwC3
+ZgvtSyV0CMFyoNwjcF4x3TmLcI6vrV5Uwjm3s90N9r+js2tIA0L4pYu/Rhu71Dr2
+5oYE+0u+hDI2DIhOIv6q28DJu0YTCjwYlyYUHmXZxZhep8dkHr4CLtnPTwIq91Iz
+17MKflnqIaJTegFNDv5gP1oW4jiZl9NgB/s8mBib2ibhIo+4r+smz4zGXvalGjBs
+jZF7drMI9prt6tLQ06lXBB0M2yaclA7nVFzWk5B6ni2zNLRXj6egG38Qyq0KWljD
+x3wphkoZ8zv+st6YU6h8XGsVKymHtmutnDTUYoMzD5XchAbl+HVJlto6m5DJ6Nbr
+e9RDIveBdm+kyTRi+Pr8AIQsIBhCDe+lb4yQ+H4vz/cvABEBAAGJAiUEGAEKAA8F
+AlcSZZMCGwwFCQHhM4AACgkQb9FfSxZGUMyhVxAA3+PzWtPR/C8FZpye3Ay/gg8C
+ltmWbZCIbWHfMAEXiFm1FSYY1WD8j/e2cShrt7TTak46a1GqKA/iZXnGjvyD6AVw
+IIGF1I5aw6Lnw4xc4G8KtLXrbzhBCVTtyf9SnFBnuoBp5nWsUAHDsGKAy3XqEG61
+8r4d4DGLTXMg4NsiDw6gsW6csyXiyPH8IvqvFTo2ty+TRYfxemxw7uPBWdF15AbE
+jAAlQ8SfmktMbBglAg2LcuNfLuDgjDgC/ht4gRew5ZCTbf3dsQrcWK4smy6PjNig
+FH8oAzM1rspvCktvoITrq8ZR0BsR0Iiiaf1j954qweBuZWcmQmWXBnqZp7Fuw1O3
+H0VxJNV3NQjO9C8UzisZz2jR49IPRkhcGH8ZYRs/HurMWn1vTiB3dJtDStLlU6Ag
+1qQXCoKZQQBPTa8hFk4oi/ZMo4rc0dF0Z3zIwa02NQOOO3dqPRUJIKxZCT1QTgyR
+eb7qCvcWvIGOUJlTrafnnCpjWmfVtbqfMx3KXrmFmz+kg83cbZ2ygcinW8pIc//R
+Dv+ZAlNDvVdPyI3CffcxHr4TopC3Pu81KTm8m8rQEsiHRb9tN0DGQH9Ncuj/z+zy
+4wpGrtqVVn+cf5ybL9hCmyrsQmU+m0Fj9XPSff//ed4byshOFuf0Y5G6Udaibce9
+/Fnpkj6YM0jLJ11p8OM=
+=zZhM
 -----END PGP PUBLIC KEY BLOCK-----
 </textarea><br>
 <script>
@@ -143,17 +135,15 @@ if (document.all && (key = document.all.key) && key.createTextRange)
 <hr>
 
 <p>If you haven't got PGP installed you can send me secure mail through this web 
-form. Since I haven't set up a proper CA, it doesn't provide authentication, but 
-it does provide end-to-end encryption. The message entered here will be
-encrypted with my public key and delivered to my email address. The From: and
-Subject: fields are optional and are sent as plaintext.</p>
+form. The message entered here will be
+encrypted with my public key and delivered to my email address.</p>
 
-<?
-  if ($HTTP_SERVER_VARS["HTTPS"] != "on")
-    print '<h3><a href="https://russ.hn.org/pgp.php" style="text-decoration:none"><font color=red>Click here to connect to a secure web server</font></a></h3>';
+<?php
+  if (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+    print '<h3><a href="https://russ.yanofsky.org/pgp.php" style="text-decoration:none"><font color=red>Click here to connect to a secure web server</font></a></h3>';
 ?>
 
-<form method=post action="<?= $HTTP_SERVER_VARS["HTTPS"] != "on" ? "https://russ.hn.org/pgp.php" : "pgp.php"?>">
+<form method=post<?= !isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on" ? ' action="https://russ.yanofsky.org/pgp.php"' : ''?>>
 <table>
 <tr>
   <td><label for=from>From:</label></td>
@@ -174,7 +164,7 @@ Subject: fields are optional and are sent as plaintext.</p>
 </table>
 </form>
 
-<? } ?>
+<?php } ?>
 
 </body>
 </html>
